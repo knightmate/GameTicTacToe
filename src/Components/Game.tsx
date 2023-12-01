2825cbdb1aefb2110fcc305ffba586a2f20c016e
Meghraj Deshmukh <meghraj.deshmukh103@gmail.com>
2023-12-02 02:48:25 +0530

    working fine :Basic logic for game

--- src/Components/Game.tsx
+++ src/Components/Game.tsx
@@ -23,6 +23,16 @@ const CellContainer = styled.div`
 `;
 
  
+interface WinningTextProps {
+    winner: string | null;
+  }
+  
+  const LargeWinningText = styled.h1<WinningTextProps>`
+    font-size: 36px;
+    text-align: center;
+    color: ${({ winner }) => (winner === 'X' ? 'Green' : 'Green')}; // Customize colors as needed
+  `;
+  
 
 // Game component
 const GameContainer = styled.div`
@@ -44,8 +54,19 @@ const Game: React.FC = () => {
     [null, null, null],
   ]);
    
-   const [gameStatus,setGameStatus]=useState<GameStart>({start:true,symbol:"x"});
- 
+   const [gameStatus,setGameStatus]=useState<GameStart>({start:false,symbol:"x"});
+   const [winner,setWinner]=useState<string|null>(null);
+
+   useEffect(()=>{
+
+    if(!gameStatus.start)return;
+
+    if(checkGameState()){
+        
+        setWinner(gameStatus.symbol)
+    }
+
+   },[matrix]);
 
   function onClick(rowIndex: number, colIndex: number){
 
@@ -57,6 +78,57 @@ const Game: React.FC = () => {
      setGameStatus({start:true,symbol:gameStatus.symbol=="o"?"x":"o"})
 
   }
+
+  function doesArrayHasAllSameElement(arr:[]){
+      
+ 
+            
+             for(let i=0;i<arr.length-1;i++){
+                 
+              if(arr[i]!=arr[i+1] || arr[i]==null) return false; 
+             }
+
+             return true;
+
+  }
+
+  function checkGameState(){
+
+       //Check in FirstLine
+
+    
+       const firstLine  =matrix[0];
+       const secondLine =matrix[1];
+       const thirdLine  =matrix[2];
+
+        
+       if(doesArrayHasAllSameElement(firstLine))return true;
+
+       console.log("arr",firstLine,doesArrayHasAllSameElement(firstLine));
+
+       if(doesArrayHasAllSameElement(secondLine))return true;
+
+       if(doesArrayHasAllSameElement(thirdLine))return true;
+
+       //check in RightLine
+       const lastColumn  =[matrix[0][2],matrix[1][2],matrix[2][2]]
+
+       if(doesArrayHasAllSameElement(lastColumn))return true;
+
+
+       //Check Diagonally
+
+       const diagonal  =[matrix[0][2],matrix[1][1],matrix[2][0]]
+
+       if(doesArrayHasAllSameElement(diagonal))return true;
+
+       const diagonal2  =[matrix[0][0],matrix[1][1],matrix[2][2]]
+       
+       if(doesArrayHasAllSameElement(diagonal2))return true;
+
+       return false;
+
+  }
     
   const renderCell = (rowIndex: number, colIndex: number) => (
     <CellContainer onClick={()=>onClick(rowIndex,colIndex)}>
@@ -64,11 +136,15 @@ const Game: React.FC = () => {
     </CellContainer>
   );
 
+  
+
   return (
     <div>
       <GameContainer>
         {matrix.map((row, rowIndex) => row.map((_, colIndex) => renderCell(rowIndex, colIndex)))}
       </GameContainer>
+      <LargeWinningText winner={winner}>{winner ? `${winner} Wins!` : 'Playing..'}</LargeWinningText>;
+
     </div>
   );
 };
